c67c145b7e99626632c3dcfa789daaed18710fbe
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-07-02 22:12:02 +0700

    feat: add users controller

---
+++ controllers/users.py
@@ -0,0 +1,123 @@
+from flask import Blueprint, request
+from connectors.mysql_connector import connection
+from models.users import Users
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select, func
+
+from flask_login import login_user, logout_user, login_required, current_user
+
+users_routes = Blueprint('users_routes', __name__)
+
+@users_routes.route('/users', methods=['POST'])
+def register_user():
+    Session = sessionmaker(connection)
+    s = Session()
+
+    s.begin()
+    try:
+        NewUser = Users(username = request.form['username'], email = request.form['email'])
+        NewUser.set_password(request.form['password_hash'])
+
+        s.add(NewUser)
+        s.commit()
+    except Exception as e:
+        print(e)
+        s.rollback
+        return {'message': 'Fail to register'}, 500
+    
+    return {'message': 'Register user success'}, 200
+    
+@users_routes.route('/users/login', methods=['POST'])
+def user_login():
+    Session = sessionmaker(connection)
+    s = Session()
+
+    s.begin()
+    try:
+        email = request.form['email']
+        user = s.query(Users).filter(Users.email == email).first()
+
+        if user == None:
+            return {'message': 'User not found'}, 403
+
+        if not user.check_password(request.form['password_hash']):
+            return {'message': 'Invalid password'}, 403
+        
+        login_user(user)
+        session_id = request.cookies.get('session')
+        return {
+            'session_id': session_id,
+            'message': 'Login success'
+        }, 200
+    
+    except Exception as e:
+        s.rollback
+        return {'message': 'Fail to login'}, 500
+    
+@users_routes.route('/users/me', methods=['GET'])
+# @login_required
+def info_user():
+    try:
+        return {
+        'username': current_user.username,
+        'email': current_user.email
+        }
+    except Exception as e:
+        print(e)
+        return {'message': 'Unauthorized'}, 401
+
+@users_routes.route('/users/me', methods=['PUT'])
+@login_required
+def update_user():
+
+    flag = False
+
+    Session = sessionmaker(connection)
+    s = Session()
+
+    s.begin()
+    try:
+        user = s.query(Users).filter(Users.id == current_user.id).first()
+
+        if 'username' in request.form:
+            user.username = request.form['username']
+            flag = True
+        if 'email' in request.form:        
+            user.email = request.form['email']
+            flag = True
+        if flag:
+            user.updated_at = func.now()
+            s.add(user)
+            s.commit()
+    
+    except Exception as e:
+        s.rollback()
+        return {'message': 'Fail to update user'}, 500
+    
+    return {'message': 'Update user info success'}, 200
+
+@users_routes.route('/users/delete', methods=['DELETE'])
+@login_required
+def delete_user():
+    Session = sessionmaker(connection)
+    s = Session()
+    
+    s.begin()
+    try:
+        user = s.query(Users).filter(Users.id == current_user.id).first()
+        s.delete(user)
+        s.commit()
+        logout_user()
+    except Exception as e:
+        s.rollback()
+        return {'message': 'Fail to delete user'}, 500
+    
+    return {'message': 'Delete user success'}, 200
+
+@users_routes.route('/users/logout', methods=['GET'])
+@login_required
+def user_logout():
+    logout_user()
+    return {'message': 'Logout user success'}, 200
+        
--- index.py
+++ index.py
@@ -1,5 +1,13 @@
 from flask import Flask
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
+from connectors.mysql_connector import connection
+
+from sqlalchemy.orm import sessionmaker
+
+from controllers.users import users_routes
+
+from flask_login import LoginManager
+from models.users import Users
 
 import os
 
@@ -7,6 +15,17 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+app.register_blueprint(users_routes)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    Session = sessionmaker(connection)
+    s = Session()
+    return s.query(Users).get(int(user_id))
 
 @app.route("/")
 def hello_world():
