42d8a7a6b4b61918af9ed4453c55e0afe26716a0
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-07-05 17:40:03 +0700

    feat: add user validation schema

---
+++ validations/users_validation.py
@@ -0,0 +1,16 @@
+users_register_schema = {
+    'username': {'type': 'string', 'required': True, 'minlength': 2},
+    'email': { 
+        'type': 'string',
+        'regex': '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$',
+        'required': True
+    }
+}
+
+users_update_schema = {
+    'username': {'type': 'string', 'minlength': 2},
+    'email': { 
+        'type': 'string',
+        'regex': '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'
+    }
+}
