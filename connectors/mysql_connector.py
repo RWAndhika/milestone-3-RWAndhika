820df2482e0da2ad193dd5a2c1fad46956ab6400
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-10-07 21:00:13 +0700

    feat: connect to deployed database hosted at filess.io

--- connectors/mysql_connector.py
+++ connectors/mysql_connector.py
@@ -5,10 +5,11 @@ username = os.getenv("DB_USERNAME")
 password = os.getenv("DB_PASSWORD")
 host = os.getenv("DB_HOST")
 database = os.getenv("DB_DATABASE")
+port = os.getenv("DB_PORT")
 
 # Connect to Database
 print("Connecting to MySQL Database")
-engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}/{database}')
+engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}')
 
 connection = engine.connect()
 print("Success connecting to MySQL Database")
